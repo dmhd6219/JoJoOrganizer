ab303e7690cb918701ba5b04eb7e269c0801f250
win9z <win9z@yandex.ru>
2020-11-07 13:48:34 +0300

    фикс

--- MainProgram.py
+++ MainProgram.py
@@ -2,23 +2,17 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from Windows.MainWindow import MyMainWindow
 
-<<<<<<< HEAD
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
-=======
-# except hook
-# def except_hook(cls, exception, traceback):
-# sys.__excepthook__(cls, exception, traceback)
->>>>>>> branch 'main' of https://github.com/Chimnay/imranhello.git
 
 
 # основной запуск программы :)
 app = QApplication(sys.argv)
 
 mainWindow = MyMainWindow()
-mainWindow.show()
+exиз четырех треугольников.show()
 
-# sys.excepthook = except_hook
+sys.excepthook = except_hook
 
 sys.exit(app.exec())
